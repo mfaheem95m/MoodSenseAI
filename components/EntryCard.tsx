cf0304a045a9db1e3950e93cfb8a4c524c095803
Muhammad Faheem <mfaheem95m@gmail.com>
2025-03-05 16:07:13 +0500

    Added layout with header & sidebar, created Journal page

---
+++ components/EntryCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const EntryCard = ({ entry }) => {
+  return <div>{entry.id}</div>
+}
+
+export default EntryCard
---
+++ components/NewEntryCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewEntryCard = () => {
+  return (
+    <div
+      className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
+      //   onClick={handleOnClick}
+    >
+      <div className="px-4 py-5 sm:p-6">
+        <span className="text-3xl">New Entry</span>
+      </div>
+    </div>
+  )
+}
+
+export default NewEntryCard
